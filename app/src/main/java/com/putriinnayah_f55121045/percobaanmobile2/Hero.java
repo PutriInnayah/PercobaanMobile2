08de18c099a7da529dd32a553cba852283ebbfb7
Putri Innayah <jy.putriii@gmail.com>
2022-10-18 19:52:21 +0700

    Initial commit

---
+++ app/src/main/java/com/putriinnayah_f55121045/percobaanmobile2/Hero.java
@@ -0,0 +1,31 @@
+package com.putriinnayah_f55121045.percobaanmobile2;
+
+class Hero {
+    private int photo;
+    private String name;
+    private String description;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return String.valueOf(name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return String.valueOf(description);
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/putriinnayah_f55121045/percobaanmobile2/TugasNaya.java
@@ -0,0 +1,14 @@
+package com.putriinnayah_f55121045.percobaanmobile2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TugasNaya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas_naya);
+    }
+}
